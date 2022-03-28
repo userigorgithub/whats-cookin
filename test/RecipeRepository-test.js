e17c1dd9412b3c5413d0a49ad0c5f8d43a996d33
Igor Decess <i.decess@comcast.net>
2022-03-28 16:52:10 -0600

    Add instance of RecipeRepository

--- test/RecipeRepository-test.js
+++ test/RecipeRepository-test.js
@@ -1,8 +1,20 @@
 import { expect } from 'chai';
 import RecipeRepository from '../src/classes/RecipeRepository';
 
-describe('Recipe', () => {
-  it('Should be a function', () => {
+describe('Recipe Repository', () => {
+
+  let recipeRepository;
+
+  beforeEach(() => {
+
+    recipeRepository = new RecipeRepository();
+  });
+
+  it('should be a function', () => {
     expect(RecipeRepository).to.be.a('function');
   });
-})
+
+  it('should be an instance of RecipeRepository', () => {
+    expect(recipeRepository).to.be.an.instanceOf(RecipeRepository);
+  });
+});
