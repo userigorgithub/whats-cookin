ae5de73a106d0b8db472a02fdeb5c71aec281408
Josh Mallery <josh.mallery@gmail.com>
2022-03-30 17:56:55 -0600

    Complete Query Selectors and get Elements by Id

--- src/scripts.js
+++ src/scripts.js
@@ -12,5 +12,24 @@ const box3 = document.getElementById('boxThree');
 
 const forwardButton = document.getElementById('goForward');
 const backwardButton = document.getElementById('goBackward');
+const recipeName = document.querySelector('.recipe-name');
+const recipeImage = document.querySelector('.recipe-image');
+const favoriteButton = document.getElementById('addFavorite');
+const wantToCookButton = document.getElementById('wantToCook');
+
+const homeButton = document.getElementById('homeButton');
+const favoritesButton = document.getElementById('favoritesButton');
+const wantToCookButton = document.getElementById('wantToCookButton');
+
+const instructions = document.querySelector('.instructions');
+const ingredients = document.querySelector('.ingredients');
+const costRecipe = document.querySelector('.cost-recipe');
+
+const searchBar = document.getElementById('search');
+
+const sideDish = document.getElementById('sideDish');
+const mainDish = document.getElementById('mainDish');
+const dessert = document.getElementById('dessert');
+
 
 console.log('hello world');
