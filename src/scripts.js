69a2bdd1d87f8925ed352f2b75915ebc16254986
Josh Mallery <josh.mallery@gmail.com>
2022-03-30 17:34:33 -0600

    Import Classes and Start DOM Manipulation

--- src/scripts.js
+++ src/scripts.js
@@ -1,8 +1,16 @@
 import './styles.css';
 import apiCalls from './apiCalls';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
+import './images/turing-logo.png';
+import Recipe from '../src/classes/Recipe';
+import Ingredient from '../src/classes/Ingredient';
+import RecipeRepository from '../src/classes/RecipeRepository';
 
+const box1 = document.getElementById('boxOne');
+const box2 = document.getElementById('boxTwo');
+const box3 = document.getElementById('boxThree');
 
+const forwardButton = document.getElementById('goForward');
+const backwardButton = document.getElementById('goBackward');
 
-console.log('Hello world');
+console.log('hello world');
