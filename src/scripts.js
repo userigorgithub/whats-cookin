63b7ebaac9eb090c1656a05bbfbafc038b56c364
Josh Mallery <josh.mallery@gmail.com>
2022-03-30 18:59:05 -0600

    Begin page Load functionality

--- src/scripts.js
+++ src/scripts.js
@@ -5,17 +5,19 @@ import './images/turing-logo.png';
 import Recipe from '../src/classes/Recipe';
 import Ingredient from '../src/classes/Ingredient';
 import RecipeRepository from '../src/classes/RecipeRepository';
+import recipeData from '../src/data/recipes.js';
 
 const box1 = document.getElementById('boxOne');
 const box2 = document.getElementById('boxTwo');
 const box3 = document.getElementById('boxThree');
+const box4 = document.getElementById('boxFour');
 
 const forwardButton = document.getElementById('goForward');
 const backwardButton = document.getElementById('goBackward');
 const recipeName = document.querySelector('.recipe-name');
 const recipeImage = document.querySelector('.recipe-image');
 const favoriteButton = document.getElementById('addFavorite');
-const wantToCookButton = document.getElementById('wantToCook');
+const addToCookButton = document.getElementById('addToCook');
 
 const homeButton = document.getElementById('homeButton');
 const favoritesButton = document.getElementById('favoritesButton');
@@ -31,5 +33,13 @@ const sideDish = document.getElementById('sideDish');
 const mainDish = document.getElementById('mainDish');
 const dessert = document.getElementById('dessert');
 
+const displayAllRecipes = () => {
+  const allRecipes = new RecipeRepository(recipeData).repositoryData;
+  allRecipes.sort((a, b) => 0.5 - Math.random());
+
+}
+
 
 console.log('hello world');
+
+window.addEventListener('load', displayAllRecipes);
