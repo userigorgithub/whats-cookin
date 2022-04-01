59e61b02329c581d4556b49a57b14b63110da0dd
Jordan Sullivan <helloandhi.jp@gmail.com>
2022-03-31 19:07:55 -0600

    Begin searchBar functionality

--- src/scripts.js
+++ src/scripts.js
@@ -1,54 +1,54 @@
-import './styles.css';
-import apiCalls from './apiCalls';
+import "./styles.css";
+import apiCalls from "./apiCalls";
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png';
-import Recipe from '../src/classes/Recipe';
-import Ingredient from '../src/classes/Ingredient';
-import RecipeRepository from '../src/classes/RecipeRepository';
-import recipeData from '../src/data/recipes.js';
+import "./images/turing-logo.png";
+import Recipe from "../src/classes/Recipe";
+import Ingredient from "../src/classes/Ingredient";
+import RecipeRepository from "../src/classes/RecipeRepository";
+import recipeData from "../src/data/recipes.js";
 
-const box1 = document.getElementById('boxOne');
-const box2 = document.getElementById('boxTwo');
-const box3 = document.getElementById('boxThree');
-const box4 = document.getElementById('boxFour');
+const box1 = document.getElementById("boxOne");
+const box2 = document.getElementById("boxTwo");
+const box3 = document.getElementById("boxThree");
+const box4 = document.getElementById("boxFour");
 
-const mainSection = document.querySelector('.main-section');
-const recipeView = document.querySelector('.recipe-view');
-const pageTitle = document.querySelector('.page-title-section');
+const mainSection = document.querySelector(".main-section");
+const recipeView = document.querySelector(".recipe-view");
+const pageTitle = document.querySelector(".page-title-section");
 
-const forwardButton = document.getElementById('goForward');
-const backwardButton = document.getElementById('goBackward');
-const boxOfRecipes = document.querySelector('.box-of-recipes');
-const recipeName = document.querySelector('.recipe-name');
-const recipeImage = document.querySelector('.recipe-image');
-const favoriteButton = document.getElementById('addFavorite');
-const addToCookButton = document.getElementById('addToCook');
+const forwardButton = document.getElementById("goForward");
+const backwardButton = document.getElementById("goBackward");
+const boxOfRecipes = document.querySelector(".box-of-recipes");
+const recipeName = document.querySelector(".recipe-name");
+const recipeImage = document.querySelector(".recipe-image");
+const favoriteButton = document.getElementById("addFavorite");
+const addToCookButton = document.getElementById("addToCook");
 
-const homeButton = document.getElementById('homeButton');
-const favoritesButton = document.getElementById('favoritesButton');
-const wantToCookButton = document.getElementById('wantToCookButton');
+const homeButton = document.getElementById("homeButton");
+const favoritesButton = document.getElementById("favoritesButton");
+const wantToCookButton = document.getElementById("wantToCookButton");
 
-const instructions = document.querySelector('.instructions');
-const ingredients = document.querySelector('.ingredients');
-const costRecipe = document.querySelector('.cost-recipe');
+const instructions = document.querySelector(".instructions");
+const ingredients = document.querySelector(".ingredients");
+const costRecipe = document.querySelector(".cost-recipe");
 
-const searchBar = document.getElementById('search');
+const searchBar = document.getElementById("search");
 
-const sideDish = document.getElementById('sideDish');
-const mainDish = document.getElementById('mainDish');
-const dessert = document.getElementById('dessert');
+const sideDish = document.getElementById("sideDish");
+const mainDish = document.getElementById("mainDish");
+const dessert = document.getElementById("dessert");
 
 const allRecipes = new RecipeRepository(recipeData).repositoryData;
-    allRecipes.sort((a, b) => 0.5 - Math.random());
+allRecipes.sort((a, b) => 0.5 - Math.random());
 
 const displayAllRecipes = () => {
-  boxOfRecipes.innerHTML ="";
+  boxOfRecipes.innerHTML = "";
 
   let showInDom = allRecipes
-  .filter((recipe,index)=> (index <= 2))
-  .map((recipe,mapIndex)=>
-    boxOfRecipes.innerHTML +=
-    `<section class="recipe-boxes" id="${recipe.id}">
+    .filter((recipe, index) => index <= 2)
+    .map(
+      (recipe, mapIndex) =>
+        (boxOfRecipes.innerHTML += `<section class="recipe-boxes" id="${recipe.id}">
       <h3 class="recipe-name">${recipe.name}</h3>
       <img class="recipe-image" id=${mapIndex} src="${recipe.image}" alt="recipe image" />
       <section class="recipe-actions"
@@ -59,28 +59,28 @@ const displayAllRecipes = () => {
           Favorite
         </button>
       </section>
-    </section>`
-  )
+    </section>`)
+    );
 };
 
 const shiftForward = () => {
-    allRecipes.push(allRecipes[0]);
-    allRecipes.shift()
-    displayAllRecipes();
-}
+  allRecipes.push(allRecipes[0]);
+  allRecipes.shift();
+  displayAllRecipes();
+};
 
 const shiftBackward = () => {
-  allRecipes.unshift(allRecipes[allRecipes.length-1])
+  allRecipes.unshift(allRecipes[allRecipes.length - 1]);
   allRecipes.pop();
   displayAllRecipes();
-}
+};
 
 const goHome = () => {
   hideElement(homeButton);
   hideElement(recipeView);
   showElement(mainSection);
-  pageTitle.innerText = `Let's Find a Recipe!`
-}
+  pageTitle.innerText = `Let's Find a Recipe!`;
+};
 
 const selectRecipe = (selectedIndex) => {
   const selectedRecipe = new Recipe(allRecipes[selectedIndex]);
@@ -90,12 +90,14 @@ const selectRecipe = (selectedIndex) => {
   showElement(recipeView);
   showElement(homeButton);
 
-recipeView.innerHTML = ""
+  recipeView.innerHTML = "";
 
-recipeView.innerHTML = `
+  recipeView.innerHTML = `
   <section class="recipe-boxes" id="boxFour">
     <h3 class="recipe-name">${selectedRecipe.singleRecipe.name}</h3>
-    <img class="recipe-image" src="${selectedRecipe.singleRecipe.image}" alt="recipe image" />
+    <img class="recipe-image" src="${
+      selectedRecipe.singleRecipe.image
+    }" alt="recipe image" />
     <section class="recipe-actions">
       <button class="recipe-action-buttons" id="addToCook">
         Add to Cook
@@ -115,32 +117,51 @@ recipeView.innerHTML = `
         <article class="other-cost">Other Cost</article>
       </section>
     </section>
-    `
+    `;
   // when clicking a recipe
-    //how do we listen to the recipes? which of the three?
+  //how do we listen to the recipes? which of the three?
 
-//if image that was clicked on, includes this id in the same object, return entire object
-
-
+  //if image that was clicked on, includes this id in the same object, return entire object
 
   //we need to access the instructions and ingredients
-    //make a new Recipe instance?
+  //make a new Recipe instance?
 
   // we need to know which box was clicked as well
   //we need to diplay the calculated cost
-    //this is in
+  //this is in
   //css with the bottom section as well
-}
+};
 const showElement = (element) => {
-  element.classList.remove('hidden');
-}
+  element.classList.remove("hidden");
+};
 
 const hideElement = (element) => {
-  element.classList.add('hidden');
-}
+  element.classList.add("hidden");
+};
 
-window.addEventListener('load', e => displayAllRecipes());
-forwardButton.addEventListener('click', e => shiftForward());
-backwardButton.addEventListener('click', e => shiftBackward());
-boxOfRecipes.addEventListener('click', e => selectRecipe(event.target.id));
-homeButton.addEventListener('click', e => goHome());
+const userSearch = (searchText) => {
+  if (event.target.className.includes("search-input")) {
+    allRecipes.filterByTag(event.target.value);
+    allRecipes.filterByName(event.target.value);
+  } else {
+    mainSection.innerText =
+      "Sorry, we couldn't find what you're looking for, please try again.";
+  }
+  //send searchBar.value into userSearch
+  //params args
+  //how to reference
+};
+//
+
+window.addEventListener("load", (e) => displayAllRecipes());
+forwardButton.addEventListener("click", (e) => shiftForward());
+backwardButton.addEventListener("click", (e) => shiftBackward());
+boxOfRecipes.addEventListener("click", (e) => selectRecipe(event.target.id));
+homeButton.addEventListener("click", (e) => goHome());
+
+searchBar.addEventListener("keypress", function (e) {
+  // if (e.key === "Enter") {
+  console.log(searchBar.value);
+  //}
+});
+//userSearch(searchText));
