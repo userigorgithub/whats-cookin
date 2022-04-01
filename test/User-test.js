dac9c9204b200997a7d9a1f0b8e9121e699c30f7
Igor Decess <i.decess@comcast.net>
2022-04-01 12:03:04 -0600

    Add name in User class

--- test/User-test.js
+++ test/User-test.js
@@ -22,4 +22,9 @@ describe('User', () => {
   it('should hold a single user', () => {
     expect(user.singleUser).to.equal(usersData[0]);
   });
+
+  it('should have a name', () => {
+	expect(user.singleUser.name).to.equal('Saige O\'Kon');
+  });
+
 });
