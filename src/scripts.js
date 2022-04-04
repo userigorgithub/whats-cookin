1e6fded61162c5b0271944b600d1c0971ba8a552
Josh Mallery <josh.mallery@gmail.com>
2022-04-03 18:27:46 -0600

    Indent and refactor scripts File

--- src/scripts.js
+++ src/scripts.js
@@ -1,7 +1,6 @@
 import "./styles.css";
 import apiCalls from "./apiCalls";
 import {fetchAll, apiUsersData, apiIngredientsData, apiRecipeData} from "./apiCalls.js";
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import "./images/logo.png";
 import "./images/search.png";
 import "./images/add.png";
@@ -12,7 +11,6 @@ import Recipe from "../src/classes/Recipe";
 import Ingredient from "../src/classes/Ingredient";
 import RecipeRepository from "../src/classes/RecipeRepository";
 import User from "../src/classes/User";
-
 //----------Query Selectors----------//
 const mainSection = document.querySelector(".main-section");
 const recipeView = document.querySelector(".recipe-view");
@@ -30,10 +28,9 @@ const instructions = document.querySelector(".instructions");
 const ingredients = document.querySelector(".ingredients");
 const costRecipe = document.querySelector(".cost-recipe");
 const searchBar = document.getElementById("search");
-
 //---------Global Variables----------//
 let recipeData, usersData, ingredientsData, currentRecipes, allRecipes, randomUser;
-
+//----------Functions----------//
 const loadPage = () => {
   console.log('scripts JS loadFetch is working')
   fetchAll()
@@ -42,41 +39,25 @@ const loadPage = () => {
 }
 
 const setGlobalVariablesAndDisplay = (data) => {
-usersData = data[0].usersData;
-ingredientsData = data[1].ingredientsData;
-recipeData = data[2].recipeData;
-console.log('recipe data within setglobal variables',recipeData)
+  usersData = data[0].usersData;
+  ingredientsData = data[1].ingredientsData;
+  recipeData = data[2].recipeData;
+  console.log('recipe data within setglobal variables',recipeData)
 
-allRecipes = new RecipeRepository(recipeData);
-allRecipes.addDefaultPreferences();
-allRecipes.repositoryData.sort((a, b) => 0.5 - Math.random());
-console.log("line 39", allRecipes);
- currentRecipes = allRecipes.repositoryData;
+  allRecipes = new RecipeRepository(recipeData);
+  allRecipes.addDefaultPreferences();
+  allRecipes.repositoryData.sort((a, b) => 0.5 - Math.random());
+  console.log("line 39", allRecipes);
+  currentRecipes = allRecipes.repositoryData;
 
- randomUser = new User(
-  usersData[Math.floor(Math.random() * usersData.length)]
-)
+  randomUser = new User(usersData[Math.floor(Math.random() * usersData.length)]);
 
-welcomeUser.innerText = `Welcome back, ${randomUser.returnUserFirstName()}!`;
-displayAllRecipes();
+  welcomeUser.innerText = `Welcome back, ${randomUser.returnUserFirstName()}!`;
+  displayAllRecipes();
 
 }
 console.log('recipe data globally',recipeData)
 
-//--------------------
-// console.log('we shoudl have recipe data here',recipeData)
-// const allRecipes = new RecipeRepository(recipeData);
-// allRecipes.addDefaultPreferences();
-// allRecipes.repositoryData.sort((a, b) => 0.5 - Math.random());
-// console.log("line 39", allRecipes);
-// let currentRecipes = allRecipes.repositoryData;
-//
-// const randomUser = new User(
-//   usersData[Math.floor(Math.random() * usersData.length)]
-// );
-
-
-//----------Functions----------//
 
 
 const displayAllRecipes = (currentRecipes = allRecipes.repositoryData) => {
@@ -166,28 +147,6 @@ const goToWantToCook = () => {
   displayAllRecipes(currentRecipes);
 };
 
-// const displayClicked = (currentRecipes = allRecipes.repositoryData) => {
-//   boxOfRecipes.innerHTML = "";
-//
-//   let showInDom = currentRecipes
-//     .filter((recipe, index) => index <= 2)
-//     .map(
-//       (recipe, mapIndex) =>
-//         (boxOfRecipes.innerHTML += `<section class="recipe-boxes" id="${recipe.id}">
-//       <h3 class="recipe-name">${recipe.name}</h3>
-//       <section class="recipe-image-holder"></section>
-//         <img class="recipe-image" id=${mapIndex} src="${recipe.image}" alt="recipe image" />
-//       <section class="recipe-actions">
-//         <img class="favorites-buttons" id=${mapIndex} src="./images/heart.png" alt="heart-icon"/>
-//         <img class="favorites-buttons" id=${mapIndex} src="./images/love.png" alt="love-icon"/>
-//         <img class="to-cook-buttons" id=${mapIndex} src="./images/add.png" alt="add-icon"/>
-//       </section>
-//         <img class="to-cook-buttons" id=${mapIndex} src="./images/minus.png" alt="minus-icon"/>
-//       </section>
-//     </section>`)
-//     );
-// };
-
 //-----------------------------------------
 const selectRecipe = (selectedIndex) => {
   const selectedRecipe = new Recipe(currentRecipes[selectedIndex]);
@@ -268,11 +227,9 @@ const toggleToCook = (recipe, id) => {
   console.log("line 224", recipe);
   if (randomUser.recipesToCook.includes(recipe)) {
     randomUser.deleteFromCook(recipe);
-    //addToCookButtons[id].src = "./images/add.png";
     allRecipes.repositoryData[id].addedToCook = false;
   } else {
     randomUser.addToCook(recipe);
-    //addToCookButtons[id].src = "./images/minus.png";
     allRecipes.repositoryData[id].addedToCook = true;
     console.log("line 233", allRecipes.repositoryData[id]);
   }
@@ -286,14 +243,11 @@ const toggleFavorites = (recipe, id) => {
     console.log("recipe present in faves");
     allRecipes.repositoryData[id].favorited = false;
     randomUser.deleteFromFavorites(recipe);
-    //  addToFavoritesButtons[id].src = "./images/love.png";
   } else {
     console.log("253", recipe);
     allRecipes.repositoryData[id].favorited = true;
     console.log(allRecipes.repositoryData[id]);
     randomUser.addToFavorite(allRecipes.repositoryData[id]);
-
-    //addToFavoritesButtons[id].src = "./images/heart.png";
   }
   displayAllRecipes(allRecipes.repositoryData);
 };
