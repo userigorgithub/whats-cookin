f8825f347231e0ca47aabed275f34ce7a754b758
Jordan Sullivan <helloandhi.jp@gmail.com>
2022-04-12 19:05:39 -0600

    add beginning tests to pantry class and pseudocode methods tests

--- src/classes/Pantry.js
+++ src/classes/Pantry.js
@@ -1,7 +1,36 @@
 import Ingredient from "../classes/Ingredient";
 
 class Pantry {
-  constructor() {}
+  constructor(pantry) {
+    this.pantry = pantry;
+  }
+  checkUserStock(recipe) {
+    this.pantry;
+    return true;
+    //compare user's amounts of Ingredients to amounts of // ingredients in one specific recipe
+    //compare whats in
+    // recipe to whats in user's pantry
+    //return true or false
+    //does user have ingred at all?
+    //do they have enough of each ingredient in array
+    //true, return yay message
+    //if anything false, returns message of what ingredients are still needed- need to iterate through whole array and check EACH item
+    //double data set- user stock and recipe indredients ///names and amount... logical operator needed...
+  }
+  decrementStock(recipe) {
+    //quantity.amount
+    //when cook a meal(Cook now button) it means we already have enough ingredients to cook this meal.
+    //so... checkUserStock method must be true for this method to work
+    //link: between recipe and pantry... ID ?
+    //for user: ingredient is ID number
+    //recipe data is: ingredeients.id (IN ARRAY, filter? forEach, match to user data.pantry)
+    //** POST also-  in API calls though**
+  }
+
+  incrementStock(ingredient) {
+    //by ingredients
+    //** POST also-  in API calls though**
+  }
 }
 
 export default Pantry;
--- test/Pantry-test.js
+++ test/Pantry-test.js
@@ -1,8 +1,132 @@
 import { expect } from "chai";
-import User from "../classes/User";
-import Recipe from "../classes/Recipe";
+import User from "../src/classes/User";
+import Recipe from "../src/classes/Recipe";
+import Pantry from "../src/classes/Pantry";
 
-// describe('Recipe', () => {
-//   let recipeData, recipe, recipeData2, recipe2, ingredientsData, ingredients;
-//
-//   beforeEach(() => {
+describe("Pantry", () => {
+  let recipeData, recipe, usersData, usersData2, pantry, pantry2, user, user2;
+
+  beforeEach(() => {
+    usersData = [
+      {
+        name: "Saige O'Kon",
+        id: 1,
+        pantry: [
+          {
+            ingredient: 1,
+            amount: 2,
+          },
+          {
+            ingredient: 2,
+            amount: 10,
+          },
+          {
+            ingredient: 3,
+            amount: 5,
+          },
+        ],
+      },
+    ];
+
+    usersData2 = [
+      {
+        name: "Ephraim Goyette",
+        id: 2,
+        pantry: [
+          {
+            ingredient: 2,
+            amount: 3,
+          },
+          {
+            ingredient: 3,
+            amount: 7,
+          },
+        ],
+      },
+    ];
+
+    recipeData = [
+      {
+        id: 1,
+        image: "https://spoonacular.com/recipeImages/595736-556x370.jpg",
+        ingredients: [
+          {
+            id: 1,
+            quantity: {
+              amount: 1.5,
+              unit: "c",
+            },
+          },
+          {
+            id: 2,
+            quantity: {
+              amount: 0.5,
+              unit: "tsp",
+            },
+          },
+          {
+            id: 3,
+            quantity: {
+              amount: 1,
+              unit: "large",
+            },
+          },
+        ],
+        instructions: [
+          {
+            instruction: "Cook something.",
+            number: 1,
+          },
+        ],
+        name: "Loaded Chocolate Chip Pudding Cookie Cups",
+        tags: [
+          "antipasti",
+          "side dish",
+          "snack",
+          "appetizer",
+          "antipasto",
+          "hor d'oeuvre",
+        ],
+      },
+      //update a decremented stock pantry
+    ];
+
+    user = new User(usersData[0]);
+    user2 = new User(usersData2[0]);
+    recipe = new Recipe(recipeData[0]);
+    pantry = new Pantry(user.singleUser.pantry);
+    pantry2 = new Pantry(user2.singleUser.pantry);
+  });
+
+  it("should be a function", () => {
+    expect(Pantry).to.be.a("function");
+  });
+
+  it("should be an instance of Pantry", () => {
+    expect(pantry).to.be.an.instanceOf(Pantry);
+  });
+
+  it("should be able to store a user's ingredients", () => {
+    expect(pantry.pantry).to.deep.equal(user.singleUser.pantry);
+    expect(pantry2.pantry).to.deep.equal(user2.singleUser.pantry);
+  });
+
+  // it("should check whether a user's pantry has enough ingredients to cook a certain recipe", () => {
+  //   expect(pantry.checkUserStock(recipe)).to.equal(true);
+  // });
+  //
+
+  // it("should check whether a user's pantry has enough ingredients to cook a certain recipe", () => {
+  //   expect(pantry2.checkUserStock(recipe)).to.equal(false);
+  // });
+  //
+  //should let a user know what ingredient they still need to cook that specific recipe.
+
+  // it("should be able to remove stock from a User's pantry after cooking the recipe", () => {
+  //   expect(pantry.decrementStock(recipe)).to.equal.(>>>decrementedupdatedpantry)
+  // })
+
+  // it("should be able to add a single ingredient to a User's pantry", () => {
+  //   expect(pantry.incrementStock(recipe)).to.equal.(>>>incrementedupdatedpantry)
+  // })
+});
