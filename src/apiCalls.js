2401f2292d4bd3ed31aa54ff78bd5a5dce9a2cc5
Igor Decess <i.decess@comcast.net>
2022-04-14 16:59:57 -0600

    Create new QS for error

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,3 +1,8 @@
+// 1. put in HTML <p class="js-error"></p> above script files at the bottom
+// 2. make QS for js-error - DONE
+
+const errorMsg = document.querySelector('.js-error');
+
 let apiUsersData, apiIngredientsData, apiRecipeData;
 
 const fetchData = (param) => {
