f7e5364242792a4fee2413a83c26c52a1c6220f6
Igor Decess <i.decess@comcast.net>
2022-04-15 17:08:56 -0600

    Remove HTML tag for error, refer to page title tag, pseudocode

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -9,17 +9,17 @@
 //note: subtracting upon other button (To Cook!) click? therefore...
 //note: will it create or is it separate POST request from adding?
 // 6. create .catch for POST requests -DONE
-// 7. create function to checkErrors for POST responses when user enters data -NOT DONE
+// 7. create function to checkErrors for POST responses when user enters data -DONE
 
 // -TO DO-
-// 8. create QS for submit button QS: submitIngredient (id)
+// 8. create QS for form button? submitIngredientForm- id
 // 9. create EL for submit's querySelector
 // 10. create function for POST req using form
 
 
 //----------Query Selectors----------//
-const errorMsg = document.querySelector('.js-error');
-
+const errorMsg = document.querySelector('.page-title-section');
+// #8
 
 //---------Global Variables----------//
 let apiUsersData, apiIngredientsData, apiRecipeData;
@@ -43,9 +43,11 @@ const changePantryStock = (pantryStock) => {
     body: JSON.stringify(pantryStock), // remember how HTTP can only send and receive strings, just like localStorage?
     headers: { 'Content-Type': 'application/json' }
   })
-  .then(response => checkErrors(response)) //checkErrors function
+  .then(response => checkError(response)) //checkError function
   .catch((error) => displayError(error))
 };
+
+// #10 FUNCTION
 
 
 const displayError = (error) => {
@@ -56,11 +58,17 @@ const displayError = (error) => {
   }
 };
 
-
+const checkError = (response) => {
+  if (!response.ok) {
+    throw new Error(response.statusText)
+  } else {
+    response.json()
+  }
+};
 
 
 //----------Event Listeners----------//
-
+// #9
 
 //----------Exports----------//
 export { fetchAll, apiUsersData, apiIngredientsData, apiRecipeData };
